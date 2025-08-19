a158961e8f6a4ff3edfb21f4b8e70e434490099a
Tenny <tenny_lu@phison.com>
2025-08-19 09:20:50 +0800

    Change models.

--- McpClient/Constants.cs
+++ McpClient/Constants.cs
@@ -8,5 +8,5 @@ namespace McpClient;
 
 internal static class Constants
 {
-    public static readonly List<string> LOCAL_MODELS = new List<string>() { "qwen3-4b-instruct-2507",  "llama-3.2-3b-instruct",  "gemma-3-4b-it" };
+    public static readonly List<string> LOCAL_MODELS = new List<string>() { "Qwen2.5-72B-Instruct-AWQ", "Qwen3-14B-AWQ", "qwen3-8b", "qwen3-4b-instruct-2507",  "llama-3.2-3b-instruct",  "gemma-3-4b-it" };
 }
