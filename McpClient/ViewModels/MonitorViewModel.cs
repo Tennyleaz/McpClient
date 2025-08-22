aa01baddd3e37ef9262fbb7eb0fcb4e000c3c2f8
Tenny <tenny_lu@phison.com>
2025-08-22 17:57:37 +0800

    Add CPU/GPU monitor window with livecharts2.

---
+++ McpClient/ViewModels/MonitorViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using SkiaSharp;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+
+namespace McpClient.ViewModels;
+
+internal class MonitorViewModel : ReactiveObject
+{
+    public ObservableCollection<ColumnSeries<int>> Series { get; set; } = new();
+
+    public Axis[] XAxes { get; set; } =
+    [
+        new Axis
+        {
+            Labels = ["CPU", "GPU"],
+            LabelsRotation = 0,
+            //SeparatorsPaint = new SolidColorPaint(new SKColor(200, 200, 200)),
+            SeparatorsAtCenter = false,
+            //TicksPaint = new SolidColorPaint(new SKColor(35, 35, 35)),
+            TicksAtCenter = true,
+            // By default the axis tries to optimize the number of 
+            // labels to fit the available space, 
+            // when you need to force the axis to show all the labels then you must: 
+            ForceStepToMin = true,
+            MinStep = 1
+        }
+    ];
+
+    public Axis[] YAxes { get; set; } =
+    [
+        new Axis
+        {
+            MinLimit = 0,
+            MaxLimit = 100,
+            // By default the axis tries to optimize the number of 
+            // labels to fit the available space, 
+            // when you need to force the axis to show all the labels then you must: 
+            ForceStepToMin = true,
+            MinStep = 20
+        }
+    ];
+}
