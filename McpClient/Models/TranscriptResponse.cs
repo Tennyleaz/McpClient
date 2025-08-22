22a5dd494947e5cdb12800bada5ce5b278fc5ae4
Tenny <tenny_lu@phison.com>
2025-08-22 17:58:52 +0800

    Add transcription data type.

---
+++ McpClient/Models/TranscriptResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McpClient.Models;
+
+internal class TranscriptResponse
+{
+    public List<Transcript> Transcript { get; set; }
+    public string Note { get; set; }
+}
+
+internal class Transcript
+{
+    public DateTime Timestamp { get; set; }
+    public string Text { get; set; }
+}
