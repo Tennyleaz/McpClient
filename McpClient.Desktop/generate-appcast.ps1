ad17dc4f4310c8b99dd7e13170e6106080dd6cf0
Tenny <tenny_lu@phison.com>
2025-10-28 09:40:26 +0800

    Set how to generate innosetup appcast.

--- McpClient.Desktop/generate-appcast.ps1
+++ McpClient.Desktop/generate-appcast.ps1
@@ -1,1 +1,4 @@
- netsparkle-generate-appcast -a "D:\workspace\output\appcast-win-x64" -e exe -b "D:\workspace\output\McpClient-win-x64" -o windows -u http://192.168.41.60:7235/chat/file/download/ -n "AI Nexus Desktop"
+netsparkle-generate-appcast -a "D:\workspace\output\appcast-win-x64" -e exe -b "D:\workspace\output\McpClient-win-setup" -o windows -u http://192.168.41.173:8080/ -n "AI Nexus Desktop"
+
+ # Get version from the installer, see:
+ # https://github.com/NetSparkleUpdater/NetSparkle/issues/306
